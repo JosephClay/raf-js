79842012b44aa87aec18355f7237a04f04a9b4c7
Joseph Clay <testingriven@gmail.com>
2015-01-17 13:27:48 -0600

    fix reference to window in browserify environments

--- src/polyfill.js
+++ src/polyfill.js
@@ -2,34 +2,34 @@
 
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- 
+
 // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
 // MIT license
- 
-var window = window,
+
+var win = window,
     lastTime = 0,
     vendors = ['ms', 'moz', 'webkit', 'o'];
-for (var idx = 0; idx < vendors.length && !window.requestAnimationFrame; ++idx) {
-    window.requestAnimationFrame = window[vendors[idx]+'RequestAnimationFrame'];
-    window.cancelAnimationFrame = window[vendors[idx]+'CancelAnimationFrame'] || window[vendors[idx]+'CancelRequestAnimationFrame'];
+for (var idx = 0; idx < vendors.length && !win.requestAnimationFrame; ++idx) {
+    win.requestAnimationFrame = win[vendors[idx]+'RequestAnimationFrame'];
+    win.cancelAnimationFrame = win[vendors[idx]+'CancelAnimationFrame'] || win[vendors[idx]+'CancelRequestAnimationFrame'];
 }
 
-if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function(callback, element) {
+if (!win.requestAnimationFrame) {
+    win.requestAnimationFrame = function(callback, element) {
         var currTime   = +(new Date()),
             timeToCall = Math.max(0, 16 - (currTime - lastTime)),
             id = setTimeout(function() {
                 callback(currTime + timeToCall);
             }, timeToCall);
-        
+
         lastTime = currTime + timeToCall;
 
         return id;
     };
 }
 
-if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function(id) {
+if (!win.cancelAnimationFrame) {
+    win.cancelAnimationFrame = function(id) {
         clearTimeout(id);
     };
 }
