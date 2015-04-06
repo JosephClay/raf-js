63eedb4ad301f0efa2d729225abca5d4a9897bc4
Joseph Clay <testingriven@gmail.com>
2015-04-05 22:01:01 -0500

    add handle delay alias

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var util = require('./src/util'),
     cycle = module.exports = require('./src/cycle');
 cycle.framerate = cycle.fps = require('./src/framerate');
 util.extend(cycle, require('./src/timeout'), require('./src/interval'));
+cycle.delay = cycle.setTimeout;
