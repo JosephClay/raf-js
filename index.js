7d5805923e1a45f8fa8e7e4307a1cf2d45cce9b4
Joseph Clay <testingriven@gmail.com>
2015-04-05 21:56:43 -0500

    fix pathing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('./src/polyfill');
-var util = require('./util'),
+var util = require('./src/util'),
     cycle = module.exports = require('./src/cycle');
 cycle.framerate = cycle.fps = require('./src/framerate');
-util.extend(cycle, require('./timeouts'), require('./intervals'));
+util.extend(cycle, require('./src/timeout'), require('./src/interval'));
