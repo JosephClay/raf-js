d5c8602fe7fe91af5b9ba596c77f78022a64ae34
Joseph Clay <j053phclay@gmail.com>
2015-08-06 13:05:47 -0500

    remove oldIE polyfill

--- src/polyfill.js
+++ src/polyfill.js
@@ -7,30 +7,8 @@
 // MIT license
 
 var win = global,
-    lastTime = 0,
     vendors = ['ms', 'moz', 'webkit', 'o'];
 for (var idx = 0; idx < vendors.length && !win.requestAnimationFrame; ++idx) {
     win.requestAnimationFrame = win[vendors[idx] + 'RequestAnimationFrame'];
     win.cancelAnimationFrame = win[vendors[idx] + 'CancelAnimationFrame'] || win[vendors[idx] + 'CancelRequestAnimationFrame'];
-}
-
-if (!win.requestAnimationFrame) {
-    // keep element here (unused to maintain the correct function length)
-    win.requestAnimationFrame = function(callback, element) {
-        var currTime   = +(new Date()),
-            timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-            id = setTimeout(function() {
-                callback(currTime + timeToCall);
-            }, timeToCall);
-
-        lastTime = currTime + timeToCall;
-
-        return id;
-    };
-}
-
-if (!win.cancelAnimationFrame) {
-    win.cancelAnimationFrame = function(id) {
-        clearTimeout(id);
-    };
 }
